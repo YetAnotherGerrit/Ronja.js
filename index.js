97965b5de8302efed86815b46ba5d0dcb478757f
Gerrit <97293098+YetAnotherGerrit@users.noreply.github.com>
2022-04-02 16:07:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const myTimezone = 'Europe/Berlin';
 const cMinimumPlayers = 3;
 
 
-const client = new Ronja({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES]});
+const client = new Ronja({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES] });
 
 
 client.once('ready', () => {
