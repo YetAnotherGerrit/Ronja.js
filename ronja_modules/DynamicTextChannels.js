9ed486632635ff4550206301349bfd1b61642f9c
Gerrit <97293098+YetAnotherGerrit@users.noreply.github.com>
2022-04-05 20:06:27 +0200

    Moved dynamic text channels to own module.

--- ronja_modules/DynamicTextChannels.js
+++ ronja_modules/DynamicTextChannels.js
@@ -3,16 +3,45 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const Moment = require('moment');
 
+// TODO: move to config
+const cMinimumPlayers = 3;
+
 const myExample = {
     client: null,
 
     init: function(client) {this.client = client},
 
-    hookForStartedPlaying: async function(oldPresence, newPresence, newActivity, gameCreated)  {
-        // https://discord.js.org/#/docs/discord.js/stable/class/ClientPresence
-        // https://discord.js.org/#/docs/discord.js/stable/class/Activity
-        // gameCreated = Boolean if it was new to Ronjas Gamedatabase
-        console.debug('Someone started playing a game!')
+    hookForStartedPlaying: async function(oldPresence, newPresence, newActivity, game)  {
+        if (game.channel) {
+            let gameChannel = await client.channels.fetch(game.channel);
+            gameChannel.permissionOverwrites.create(newPresence.member.user,{'VIEW_CHANNEL': true});
+        } else {
+            let players  = await client.myDB.GamesPlayed.findAndCountAll({
+                where: {GameId: game.id},
+            });
+
+            if (players.count >= cMinimumPlayers) {
+                let autoChannel = await client.channels.fetch(client.myConfig.AktiveSpieleKategorie)
+                let newChannel = await autoChannel.createChannel(newActivity.name,{
+                    type: 'GUILD_TEXT',
+                    permissionOverwrites: [
+                        {
+                            id: newPresence.guild.roles.everyone,
+                            deny: ['VIEW_CHANNEL'],
+                        },
+                        {
+                            id: newPresence.guild.me,
+                            allow: ['VIEW_CHANNEL'],
+                        },
+                    ],										
+                });
+                players.rows.forEach(async player => {
+                    let player_member = await newPresence.guild.members.fetch(player.member);
+                    newChannel.permissionOverwrites.create(player_member,{'VIEW_CHANNEL': true});
+                });
+                game.update({channel: newChannel.id});
+            };
+        };
     },
 
 
