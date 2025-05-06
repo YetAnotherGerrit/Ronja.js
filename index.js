45fa6fe8390953c741a607fd5201d4fe455691bc
Gerrit <97293098+YetAnotherGerrit@users.noreply.github.com>
2025-05-06 21:30:55 +0200

    Remove NewWorld-Support
    New World is a dying game, the API is not working anymore. We don't play New World anymore. If I ever add game-specific modules, it will be even more modularized.

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const ronja_modules = [];
 ronja_modules.push(require("./ronja_modules/Calendarfeed.js"));
 ronja_modules.push(require("./ronja_modules/DynamicTextChannels.js"));
 ronja_modules.push(require("./ronja_modules/DynamicVoiceChannels.js"));
-ronja_modules.push(require("./ronja_modules/NWDB.js"));
 ronja_modules.push(require("./ronja_modules/ReoccurringEvents.js"));
 ronja_modules.push(require("./ronja_modules/Serverprofil.js"));
 ronja_modules.push(require("./ronja_modules/SetLanguage.js"));
